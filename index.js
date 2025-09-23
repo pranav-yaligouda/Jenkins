2eff1b6be734aa2bf59465ebff82321e8d0f239c
pranav-yaligouda <pranavyaligouda7@gmail.com>
2025-09-23 14:58:42 +0530

    added helth route api endpoint /health returns status and uptime for successfull geet request

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
     res.send({ message: "Welcome to the Express Server with Jeninks CI/CD"});
 })
 
+app.get('/health', (req, res) => {
+    res.status(200).send({ status: "ok", uptime: process.uptime() });
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
 })
